13017573e47bc72b007addf29d263cb95eb8e7fd
borobor <bornajurisa@gmail.com>
2022-09-18 17:50:07 +0200

    Book adding and rendering library now works

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const library = document.querySelector('.library');
+const addBook = document.querySelector('.add');
+const sidebar = document.querySelector('.sidebar');
 
 let myLibrary = [];
 
@@ -13,8 +15,10 @@ let android = new Book('Do Androids Dream of Electric Sheep', 'Philip K. Dick', 
 
 myLibrary.push(android);
 
-
 function addBookToLibrary() {
+	while (library.firstChild) {
+		library.removeChild(library.lastChild);
+	}
 	myLibrary.forEach(book => {
 		const bookItem = document.createElement('div');
 		library.appendChild(bookItem);
@@ -28,3 +32,55 @@ function addBookToLibrary() {
 }
 
 addBookToLibrary();
+
+addBook.addEventListener("click", createForm)
+
+function createForm() {
+	form = document.createElement('form');
+	
+	const formTitle = document.createElement('input');
+	formTitle.setAttribute('type', 'text');
+	formTitle.setAttribute('ID', 'title');
+	formTitle.setAttribute('placeholder', 'Book title');
+	
+	const formAuthor = document.createElement('input');
+	formAuthor.setAttribute('type', 'text');
+	formAuthor.setAttribute('ID', 'author');
+	formAuthor.setAttribute('placeholder', 'Book author');
+
+	const formPages = document.createElement('input');
+	formPages.setAttribute('type', 'number');
+	formPages.setAttribute('ID', 'pages');
+	formPages.setAttribute('placeholder', '99');
+	
+	const formRead = document.createElement('input');
+	formRead.setAttribute('type', 'checkbox');
+	formRead.setAttribute('ID', 'read');
+
+
+	const submitButton = document.createElement('button');
+	submitButton.setAttribute('type', 'submit');
+	submitButton.textContent = 'Submit'
+
+	form.appendChild(formTitle);
+	form.appendChild(formAuthor);
+	form.appendChild(formPages);
+	form.appendChild(formRead);
+	form.appendChild(submitButton);
+	sidebar.appendChild(form);
+
+}
+
+document.addEventListener('submit', submitBook);
+
+function submitBook(event) {
+	event.preventDefault();
+	let title = document.getElementById('title').value;
+	let author = document.getElementById('author').value;
+	let pages = document.getElementById('pages').value;
+	let read = document.getElementById('read').checked;
+
+	myLibrary.push(new Book(title, author, pages, read));
+
+addBookToLibrary();
+}
